96782e7986bec73ccdb2b170bcd69e1ee3e3f40f
verssae <sparky@hanyang.ac.kr>
2021-10-15 15:12:06 +0900

    wip

--- rl/plot_rl_rewards.py
+++ rl/plot_rl_rewards.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--mode', type=str, required=True,
@@ -19,4 +21,25 @@ else:
   plt.hist(a, bins=20)
 
 plt.title(args.mode)
-plt.show()
+plt.savefig('result.png', dpi=300)
+
+webhook = DiscordWebhook(url='https://discord.com/api/webhooks/897394549129875496/zHsQBzzvbmqzMRXyolNoETpv6HmBS6-RMz_cP5AOIgDrqTr6kGXcOmECTsH8VVhnXL1V',content='Message', username="학습 종료")
+
+embed = DiscordEmbed(title='Plot', description='Reward', color='03b2f8')
+
+embed.add_embed_field(name='average reward', value=f'{a.mean():.2f}')
+embed.add_embed_field(name='min', value=f'{a.min():.2f}')
+embed.add_embed_field(name='max', value=f'{a.max():.2f}')
+
+
+# plt.savefig('result.png', dpi=300)
+
+with open('result.png', 'rb') as f:
+  webhook.add_file(file=f.read(), filename='plot.png')
+embed.set_thumbnail(url='attachment://plot.png')
+
+
+webhook.add_embed(embed)
+response = webhook.execute()
+
+# plt.show()
